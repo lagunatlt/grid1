29ecf56b0b9e860e7e10ca5e8541cc8c7dadd8f8
AndreevSA <lagunatlt@yandex.ru>
2019-09-18 23:01:33 +0400

    start

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var battt = document.querySelector('#content3').addEventListener('click', function () {
+	document.querySelector('#row2').style.justifyContent = 'left'
+})
+var batto = document.querySelector('#content4').addEventListener('click', function(){
+	document.querySelector('#row2').style.justifyContent = 'center'
+})
+var batty = document.querySelector('#content6').addEventListener('click', function () {
+	document.querySelector('#row2').style.justifyContent = 'space-between'
+})
+var battr = document.querySelector('#content5').addEventListener('click', function () {
+	document.querySelector('#row2').style.justifyContent = 'right'
+})
